76bd6301cd7a109972fc745945451d5707ecbbfb
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-05 23:13:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -8,15 +8,15 @@ if fizz_3.class == Integer
   if fizz_5.class == Integer
     return "FizzBuzz"
   end
-# elsif fizz_3.class == Integer
-#   return "Fizz"
-# end
-# elsif fizz_5.class == Integer
-#   return "Buzz"
-# end
-# else
-#   return nil
-# end
+elsif fizz_3.class == Integer
+  return "Fizz"
+end
+elsif fizz_5.class == Integer
+  return "Buzz"
+end
+else
+  return nil
+end
 end
 end
 puts fizzbuzz(15)
