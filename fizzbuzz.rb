96abe8c3b4bac92eb84ea17d4d15e4de0e77e995
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2021-01-05 22:49:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,15 +1,15 @@
 
 # require 'pry'
 def fizzbuzz number
-if number % 3 == class.fixnum == true
-  if number % 5 == class.fixnum == true
+if number / 3 == class.fixnum
+  if number / 5 == class.fixnum
     return "FizzBuzz"
   end
 end
-elsif number % 3 == class.fixnum == true
+elsif number / 3 == class.fixnum
   return "Fizz"
 end
-elsif number % 5 == class.fixnum == true
+elsif number / 5 == class.fixnum
   return "Buzz"
 end
 else
